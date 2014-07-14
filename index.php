50aaeb26285629911e8ddb8f47f18a23e0ce0845
João Pedro <joni_silvaco@hotmail.com>
2014-07-14 23:34:36 +0100

    Removing unnecessary files. Adding a theme and Font Awesome files to the header

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TugaPower</title>
 
-	<!-- Main CSS file -->
-	<link href="css/style.css" rel="stylesheet">
+    <!-- Main CSS file -->
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
