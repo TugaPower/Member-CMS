ba5b54b754ecd45a115e9d93a6a35457e365102e
João Pedro <joni_silvaco@hotmail.com>
2014-07-14 23:52:31 +0100

    Constants!

--- includes/Header.php
+++ includes/Header.php
@@ -1,10 +1,11 @@
+<?php include("./app/Constants.php"); ?>
 <!DOCTYPE html>
 <html lang="pt">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TugaPower</title>
+    <title><?php echo WEBSITE_TITLE; ?></title>
 
     <!-- Main CSS file -->
     <link href="css/style.css" rel="stylesheet">
