50b0511e30caf0629277c76b1f31ea94583392a2
João Pedro <joni_silvaco@hotmail.com>
2014-07-15 01:07:20 +0100

    Navbar madness!

--- includes/Navbar.php
+++ includes/Navbar.php
@@ -1,3 +1,19 @@
 <nav id="navbar" class="navbar navbar-inverse" role="navigation">
-    ...
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#"><i id="logo" class="fa fa-power-off"></i> <?php echo WEBSITE_TITLE ?></a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img id="avatar" src="https://avatars1.githubusercontent.com/u/4002159?s=32"></img>joaopms <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </nav>
