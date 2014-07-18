b93e814298f1e50a129eb5b3d5994a514539093a
Sandro Amaral <sandro123iv@gmail.com>
2014-07-18 03:00:58 +0100

    Added default news structure.

--- home.php
+++ home.php
@@ -1,1 +1,8 @@
-OKAERI!
+<iframe width="1280" height="720" src="//www.youtube.com/embed/P_MJbgO7SF0?rel=0" frameborder="0" allowfullscreen></iframe>
+<?php
+    $news = array(array("title" => "Título 1", "message" => "Text. More Text, blabla", "author" => "SandroHc", "date" => "idk"),
+                  array("title" => "Título 2", "message" => "Text 2. More Text 2, blabla", "author" => "joaopms", "date" => "idk"));
+
+    foreach($news as $current) {
+        echo "<div class=\"news\"><h1>". $current["title"] ."</h1>". $current["message"] ."<br><span class=\"author\">by ". $current["author"] ." on ". $current["date"] ."</span></div>";
+    }
